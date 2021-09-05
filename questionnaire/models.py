e5d1d7a3053dd2f8d6f9292be47392643c246763
Denis Orionov <orionovdv@gmail.com>
2021-09-05 22:46:53 +0300

    Add table args

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -2,6 +2,8 @@ from app import db
 
 
 class User(db.Model):
+    __table_args__ = {'schema': 'orionov'}
+
     user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(10), nullable=False)
     middle_name = db.Column(db.String(20), nullable=False)
@@ -10,6 +12,8 @@ class User(db.Model):
 
 
 class UserProfiles(db.Model):
+    __table_args__ = {'schema': 'orionov'}
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
     user = db.relationship('User', backref='user_profiles', uselist=False)
     education = db.Column(db.Text, nullable=True)
